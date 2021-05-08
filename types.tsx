26d78c9aa7a9f6b044e7cfcdc721a830b5e530d5
SoniSSK <49443497+SoniSSk@users.noreply.github.com>
2021-05-08 17:14:47 +0530

    Add files via upload

---
+++ types.tsx
@@ -0,0 +1,38 @@
+/**
+ * Learn more about using TypeScript with React Navigation:
+ * https://reactnavigation.org/docs/typescript/
+ */
+
+export type RootStackParamList = {
+  Root: undefined;
+  NotFound: undefined;
+};
+
+export type BottomTabParamList = {
+  TabOne: undefined;
+  TabTwo: undefined;
+  TabThree: undefined;
+  TabFour: undefined;
+  TabFive: undefined;
+
+};
+
+export type TabOneParamList = {
+  TabOneScreen: undefined;
+};
+
+export type TabTwoParamList = {
+  TabTwoScreen: undefined;
+};
+
+export type TabThreeParamList = {
+  TabThree: undefined;
+};
+
+export type TabFourParamList = {
+  TabFour: undefined;
+};
+
+export type TabFiveParamList = {
+  TabFive: undefined;
+};
